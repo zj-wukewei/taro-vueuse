07e15d9713b1fca9ab3a2a637f138fcf983281a4
wukewei <565389849@qq.com>
2022-05-22 17:14:47 +0800

    useImage

---
+++ packages/vueuse/src/useImage/index.md
@@ -0,0 +1,51 @@
+---
+title: useImage
+nav:
+  title: Hooks
+  path: /hooks
+  order: 2
+group:
+  title: 媒体
+  path: /media
+---
+
+# useImage
+
+图片操作, 如预览、获取、压缩等.
+
+## 何时使用
+
+当需要对图片进行操作时
+
+## API
+
+```jsx | pure
+const [
+  fileInfo,
+  { choose, chooseMessageFile, preview, save, getInfo, compress },
+] = useImage(initialOption);
+```
+
+## 参数说明
+
+| 参数          | 说明                                         | 类型                                                                               | 默认值 |
+| ------------- | -------------------------------------------- | ---------------------------------------------------------------------------------- | ------ |
+| initialOption | 初始拾取图片配置(若指定后面可与新的配置合并) | `Partial<Pick<chooseImage.Option, 'count' &#124; 'sizeType' &#124; 'sourceType'>>` | -      |
+
+## 返回值说明
+
+| 返回值            | 说明              | 类型                                                                                                                                                                    |
+| ----------------- | ----------------- | ----------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| choose            | 选择图片          | `(option?: ChooseImageOption) => Promise<chooseImage.SuccessCallbackResult>`                                                                                            |
+| chooseMessageFile | 选择会话资源      | `(count: number,type?: Pick<chooseMessageFile.Option, 'type'>,extend?: Pick<chooseMessageFileOption, 'extension'>) => Promise<chooseMessageFile.SuccessCallbackResult>` |
+| preview           | 预览图片          | `(option: PreviewImageOption) => Promise<General.CallbackResult>`                                                                                                       |
+| save              | 保存图片(h5 支持) | `(filePath: string) => Promise<General.CallbackResult>`                                                                                                                 |
+| getInfo           | 获取图片资源详情  | `(src: string) => Promise<getImageInfo.SuccessCallbackResult>`                                                                                                          |
+| compress          | 压缩图片(h5 支持) | `(src: string,quality?: number) => Promise<compressImage.SuccessCallbackResult>`                                                                                        |
+
+
+## Hook 支持度
+
+| 微信小程序 | H5  | ReactNative |
+| :--------: | :-: | :---------: |
+|     ✔️     | ✔️  |     ✔️      |
---
+++ packages/vueuse/src/useImage/index.ts
@@ -0,0 +1,219 @@
+import Taro, {
+  saveImageToPhotosAlbum,
+  previewImage,
+  getImageInfo,
+  compressImage,
+  chooseImage,
+  chooseMessageFile,
+  getEnv
+} from '@tarojs/taro';
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+import { saveImageForH5, downloadImage, compressForH5 } from './utils';
+import { ENV_TYPE } from '../constant';
+
+export type ChooseImageOption = Partial<
+  Pick<chooseImage.Option, 'count' | 'sizeType' | 'sourceType'>
+>;
+
+export type PreviewImageOption = Pick<previewImage.Option, 'current' | 'urls'>;
+
+export type ChooseImageAction = (
+  option?: ChooseImageOption,
+) => Promise<chooseImage.SuccessCallbackResult>;
+
+export type PreviewImageAction = (
+  option: PreviewImageOption,
+) => Promise<TaroGeneral.CallbackResult>;
+
+export type SaveImageToPhotosAlbumAction = (
+  filePath: string,
+) => Promise<TaroGeneral.CallbackResult>;
+
+export type GetImageInfoAction = (
+  src: string,
+) => Promise<getImageInfo.SuccessCallbackResult>;
+
+export type ChooseMessageFileAction = (
+  count: number,
+  type?: Pick<chooseMessageFile.Option, 'type'>,
+  extend?: Pick<chooseMessageFile.Option, 'extension'>,
+) => Promise<chooseMessageFile.SuccessCallbackResult>;
+
+export type CompressImageAction = (
+  src: string,
+  quality?: number,
+) => Promise<compressImage.SuccessCallbackResult>;
+
+export type IFileInfo = Ref<
+Pick<chooseImage.SuccessCallbackResult, 'tempFilePaths' | 'tempFiles'>
+>
+
+export interface IAction {
+  choose: ChooseImageAction;
+  chooseMessageFile: ChooseMessageFileAction;
+  preview: PreviewImageAction;
+  save: SaveImageToPhotosAlbumAction;
+  getInfo: GetImageInfoAction;
+  compress: CompressImageAction;
+}
+
+function useImage(options: ChooseImageOption): [IFileInfo, IAction] {
+  const fileInfoRef= ref<IFileInfo>({});
+  const env = getEnv();
+
+  const chooseImageAsync : ChooseImageAction = ((option = {}) => {
+    const { count, sizeType, sourceType } = options;
+    const finalOptions = Object.assign(
+      {},
+      Object.fromEntries(
+        [
+          ['count', count],
+          ['sizeType', sizeType],
+          ['sourceType', sourceType],
+        ].filter((v) => v[1]) || [],
+      ),
+      option || {},
+    );
+    return new Promise((resolve, reject) => {
+      try {
+        chooseImage({
+          ...finalOptions,
+          success: (res) => {
+            const { errMsg, ...fileInfo } = res;
+            fileInfoRef.value = fileInfo;
+            resolve(res);
+          },
+          fail: reject,
+        }).catch(reject);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  };
+
+  const previewImageAsync: PreviewImageAction = (option) => {
+    return new Promise((resolve, reject) => {
+      try {
+        previewImage({
+          ...option,
+          success: resolve,
+          fail: reject,
+        }).catch(reject);
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }, []);
+
+  const saveImageToPhotosAlbumAsync: SaveImageToPhotosAlbumAction = (filePath) => {
+      return new Promise(async (resolve, reject) => {
+        if (!filePath) {
+          reject('you must provide filePath');
+        } else {
+          try {
+            if (env === ENV_TYPE.WEB) {
+              const result = await saveImageForH5(filePath);
+              if (result) {
+                resolve({
+                  errMsg: 'save success',
+                });
+              } else {
+                reject('save fail');
+              }
+            } else {
+              saveImageToPhotosAlbum({
+                filePath,
+                success: resolve,
+                fail: reject,
+              }).catch(reject);
+            }
+          } catch (e) {
+            reject(e);
+          }
+        }
+      });
+    };
+
+  const getImageInfoAsync : GetImageInfoAction = (src) => {
+    return new Promise((resolve, reject) => {
+      if (!src) {
+        reject('please enter a valid path');
+      } else {
+        try {
+          getImageInfo({
+            src,
+            success: resolve,
+            fail: reject,
+          }).catch(reject);
+        } catch (e) {
+          reject(e);
+        }
+      }
+    });
+  };
+
+  const chooseMessageFileAsync: ChooseMessageFileAction =
+    (count, type, extension) => {
+      return new Promise((resolve, reject) => {
+        if (!count || env !== ENV_TYPE.WEAPP) {
+          reject('you must provide count');
+        } else {
+          try {
+            const payload = Object.fromEntries(
+              [
+                ['type', type],
+                ['extension', extension],
+              ].filter((v) => v[1]) || [],
+            );
+            Taro.chooseMessageFile({
+              count,
+              ...payload,
+              success: resolve,
+              fail: reject,
+            });
+          } catch (e) {
+            reject(e);
+          }
+        }
+      });
+    };
+
+  const compressImageAsync: CompressImageAction = 
+    (src, quality) => {
+      return new Promise(async (resolve, reject) => {
+        if (!src) {
+          reject('you must provide src');
+        }
+        try {
+          if (env === ENV_TYPE.WEB) {
+            const blob = await downloadImage(src);
+            compressForH5(blob, quality).then(resolve, reject);
+          } else {
+            Taro.compressImage({
+              src,
+              ...(quality ? { quality } : {}),
+              success: resolve,
+              fail: reject,
+            }).catch(reject);
+          }
+        } catch (e) {
+          reject(e);
+        }
+      });
+    };
+
+  return [
+    fileInfoRef as IFileInfo,
+    {
+      choose: chooseImageAsync,
+      chooseMessageFile: chooseMessageFileAsync,
+      preview: previewImageAsync,
+      save: saveImageToPhotosAlbumAsync,
+      getInfo: getImageInfoAsync,
+      compress: compressImageAsync,
+    },
+  ];
+}
+
+export default useImage;
