a4ef8af2e2674528cb9d1b8a71936e42f1248470
wukewei <565389849@qq.com>
2022-06-27 14:08:39 +0800

    useUpdateManager

---
+++ packages/vueuse/src/useUpdateManager/index.md
@@ -0,0 +1,50 @@
+---
+title: useUpdateManager
+nav:
+  title: Hooks
+  path: /hooks
+  order: 2
+group:
+  title: 小程序
+  path: /wechat
+---
+
+# useUpdateManager
+
+获取全局唯一版本更新管理器
+
+## 何时使用
+
+管理小程序更新机制。
+
+## API
+
+```jsx | pure
+const updateManager = useUpdateManager({
+  onCheckForUpdate,
+  onUpdateReady,
+  onUpdateFailed,
+});
+```
+
+## 入参说明
+
+```tsx | pure
+function onCheckForUpdate(manager, res) {}
+
+function onUpdateReady(manager) {}
+
+function onUpdateFailed(manager) {}
+```
+
+## 返回值说明
+
+| 返回值        | 说明           | 类型            |
+| ------------- | -------------- | --------------- |
+| updateManager | 更新管理器实例 | `UpdateManager` |
+
+## Hook 支持度
+
+| 微信小程序 | H5  | ReactNative |
+| :--------: | :-: | :---------: |
+|     ✔️     |     |             |
---
+++ packages/vueuse/src/useUpdateManager/index.ts
@@ -0,0 +1,67 @@
+import { getUpdateManager, UpdateManager, getEnv } from '@tarojs/taro';
+import useModal from '../useModal';
+import useToast from '../useToast';
+import { ENV_TYPE } from '../constant';
+
+export type Result = UpdateManager | undefined;
+
+export type IAction = (manager: UpdateManager) => void;
+export interface IUpdateManager {
+  onCheckForUpdate?: (manager: UpdateManager, result: UpdateManager.OnCheckForUpdateResult) => void;
+  onUpdateReady?: IAction;
+  onUpdateFailed?: IAction;
+}
+
+function useUpdateManager({
+  onCheckForUpdate,
+  onUpdateReady,
+  onUpdateFailed,
+}: IUpdateManager): Result {
+  const env = getEnv();
+  const updateManagerInstance = getUpdateManager();
+  const [{ showNoneAsync }] = useToast();
+  const [ show ] = useModal({
+    title: '更新提示',
+    content: '新版本已经准备好，是否重启应用？',
+  });
+
+  const onDefaultCheckForUpdate = (
+    manager: UpdateManager,
+    result: UpdateManager.OnCheckForUpdateResult,
+  ) => {
+    if (result.hasUpdate) {
+      showNoneAsync('检测到有新版本正在更新...');
+    }
+  };
+
+  const onDefaultUpdateReady: IAction = () => {
+    show().then(() => {
+      updateManagerInstance.applyUpdate();
+    })
+  }
+
+  const onDefaultUpdateFailed: IAction = () => {
+    showNoneAsync('新版本下载失败，请稍后重试！');
+  }
+
+  const addEventListener = () => {
+    if (env === ENV_TYPE.WEAPP) {
+      updateManagerInstance.onCheckForUpdate((result: UpdateManager.OnCheckForUpdateResult) => {
+        (onCheckForUpdate ?? onDefaultCheckForUpdate)(updateManagerInstance, result);
+      });
+      updateManagerInstance.onUpdateReady(() => {
+        (onUpdateReady ?? onDefaultUpdateReady)(updateManagerInstance);
+      });
+      updateManagerInstance.onUpdateFailed(() => {
+        (onUpdateFailed ?? onDefaultUpdateFailed)(updateManagerInstance);
+      });
+    } else {
+      console.log(`此hooks只为小程序提供，当前环境为${env}`);
+    }
+  };
+  addEventListener();
+
+  return updateManagerInstance;
+}
+
+export default useUpdateManager;
